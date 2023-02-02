a707689cdfd328c79f5753f56706e43384eba3cb
Mollika-Deb <mollika1551@gmail.com>
2023-02-02 13:55:30 +0600

    array indexing

--- Array.js
+++ Array.js
@@ -2,3 +2,8 @@ var age = [12, 34, 54, 67, 234];
 console.log(age);
 age[1] = 50;
 console.log(age);
+
+//find index of an element
+
+var positionIndex = age.indexOf(67);
+console.log(positionIndex);
