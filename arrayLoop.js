2da2168a579e7d0713bc92e34bda367e8aa8eeeb
Mollika-Deb <mollika1551@gmail.com>
2023-02-02 20:31:40 +0600

    loop

---
+++ arrayLoop.js
@@ -0,0 +1,6 @@
+var numbers = [23, 34, 54, 67, 78, 234]
+
+for(var i=0; i<6; i++){
+    number = numbers[i];
+    console.log(number);
+}
---
+++ while.js
@@ -0,0 +1,7 @@
+var number = 0;
+
+while(number < 7){
+    console.log("This number is valid");
+    console.log(number);
+    number++;
+}
